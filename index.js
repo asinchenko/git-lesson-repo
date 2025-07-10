f63239018023fbbafa280196787fb69220a145c7
Sinchenko Alexander <wonderfulsanchez@gmail.com>
2025-07-10 20:40:24 +0500

    choose a random dog with a subbreed

--- index.js
+++ index.js
@@ -5,10 +5,35 @@ const PORT = 3000
 
 app.get('/', async (req, res) => {
 	try {
-		const response = await fetch('https://dog.ceo/api/breeds/image/random')
-		const data = await response.json()
+		// 1. Get breed list
+		const breedListResponse = await fetch('https://dog.ceo/api/breeds/list/all')
+		const breedListData = await breedListResponse.json()
+		const breeds = breedListData.message
 
-		const dogImageUrl = data.message
+		// 2. Flatten breeds and sub-breeds into possible options
+		const breedOptions = []
+		for (const breed in breeds) {
+			if (breeds[breed].length === 0) {
+				breedOptions.push({ breed, subBreed: null })
+			} else {
+				breeds[breed].forEach(subBreed => {
+					breedOptions.push({ breed, subBreed })
+				})
+			}
+		}
+
+		// 3. Pick random breed option
+		const randomOption =
+			breedOptions[Math.floor(Math.random() * breedOptions.length)]
+
+		// 4. Build URL
+		const apiUrl = randomOption.subBreed
+			? `https://dog.ceo/api/breed/${randomOption.breed}/${randomOption.subBreed}/images/random`
+			: `https://dog.ceo/api/breed/${randomOption.breed}/images/random`
+
+		const imageResponse = await fetch(apiUrl)
+		const imageData = await imageResponse.json()
+		const dogImageUrl = imageData.message
 
 		res.send(`
       <html>
@@ -16,7 +41,9 @@ app.get('/', async (req, res) => {
           <title>Random Dog</title>
         </head>
         <body style="text-align: center; font-family: sans-serif;">
-          <h1>Here’s a random dog 🐶</h1>
+          <h1>Here’s a random ${
+						randomOption.subBreed ? randomOption.subBreed + ' ' : ''
+					}${randomOption.breed} 🐶</h1>
           <img src="${dogImageUrl}" alt="Random Dog" style="max-width: 600px; height: auto;" />
         </body>
       </html>
